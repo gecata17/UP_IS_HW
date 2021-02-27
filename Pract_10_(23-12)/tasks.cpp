1c49a4e5b7f61c947554337e2ebf190f57a7c25d
Georgi Birdanov <72568555+gecata17@users.noreply.github.com>
2021-02-27 19:19:35 +0200

    Update tasks.cpp

--- Pract_10_(23-12)/tasks.cpp
+++ Pract_10_(23-12)/tasks.cpp
@@ -181,3 +181,76 @@ int main()
 	return 0;
 }
 
+
+task 08 //finished
+#include<iostream>
+using namespace std;
+
+void evaluate(const char* str){
+	static int sCaps = 0;
+	static int lCaps = 0;
+	static int digits = 0;
+	
+	if(*str == '\0'){
+		cout << "Small Letters: " << sCaps << endl;
+		cout << "Capital Letters: " << lCaps << endl;
+		cout << "Digits: " << digits << endl;
+		return;
+	}
+	if(*str >= 'A' && *str <= 'Z'){
+		lCaps++;
+	}
+	if(*str >= 'a' && *str <= 'z'){
+		sCaps++;
+	}
+	if(*str >= '0' && *str <= '9'){
+		digits++;
+	}
+	
+	evaluate(str + 1);
+}
+
+int main(){
+	char str[] = "123aABbcCZ";
+	
+	evaluate(str);
+	
+	return 0;
+}
+ 
+
+task 09
+#include<iostream>
+using namespace std;
+
+bool swapcase(char *str){
+	
+	if(*str == '\0'){
+		return true;
+	}
+	if(*str >= 'A' && *str <= 'Z'){
+		*str += 32; 
+		return swapcase(str + 1);
+	}
+	if(*str >= 'a' && *str <= 'z'){
+		*str -= 32;
+		return swapcase(str + 1);
+	}
+	
+	return swapcase(str + 1);
+}
+
+
+int main(){
+	
+	char str[] = "Yellow Submarine";
+	
+    if(swapcase(str)){
+    	cout << str;
+	}
+	else{
+		cout << "Invalid input" << endl;
+	}
+	
+	return 0;
+}	
