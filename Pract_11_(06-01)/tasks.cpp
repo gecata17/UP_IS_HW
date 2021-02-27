5269f49ef29fa2faea779ada62b6de232494b9d8
Georgi Birdanov <72568555+gecata17@users.noreply.github.com>
2021-02-27 19:17:44 +0200

    Update tasks.cpp

--- Pract_11_(06-01)/tasks.cpp
+++ Pract_11_(06-01)/tasks.cpp
@@ -118,3 +118,139 @@ int NOD(int &n, int &m)
   
 	return 0;
 }
+
+
+task 01 //finished
+ #include <iostream>
+ using namespace std;
+        
+int nod ( int m, int n) {
+    int result;
+                
+    if ( n == 0 ){
+        result = m;
+    }  
+    else if ( n > m ){
+        result = nod ( n, m ) ;
+    }                
+    else {
+        result = nod ( n, m % n);
+    }               
+                
+    return result;
+}
+        
+int main() {
+    int m, n;
+                
+        do{
+            cout << " m= ";
+            cin >> m;
+
+            cout << "n = ";
+            cin >> n;
+
+        } while ( m < 0 || n < 0 || ( m == 0 && n == 0 ) );
+                
+        cout<<"\n NOD = "<< nod ( m, n ) << endl;
+
+    return 0;
+}
+
+
+task 02
+#include<iostream>
+using namespace std;
+
+bool isPresent(int n, int size, int *arr)
+{
+	if (size <= 0)
+		return false;
+
+	if (n == arr[size - 1])
+		return 1;
+
+	else isPresent(n, size - 1, arr);
+}
+
+
+int main(){
+    int arr[] = {1, 2, 3, 4, 5};
+
+    cout << isPresent(2, 5, arr);
+
+    return 0;
+}
+
+task03
+#include<iostream>
+using namespace std;
+
+bool isIn(int number, int digit)
+{
+	if (number == 0)
+		return false;
+
+	if (number % 10 == digit)
+		return true;
+
+	return isIn(number / 10, digit);
+}
+
+int main(){
+    int num = 123;
+
+    cout << isIn(2, num);
+
+    return 0;
+}
+
+
+task 04  //not finished
+#include<iostream>
+using namespace std;
+
+int ackerman(int m, int n)
+{
+	if (m == 0 && n)
+		return n + 1;
+
+	if (m && 0)
+		return ackerman(m - 1, 1);
+
+	return ackerman(m - 1, ackerman(m, n - 1));
+
+}
+
+int main(){
+    
+
+    return 0;
+}
+
+
+task 05
+#include<iostream>
+using namespace std;
+
+bool isUnique(int arr[], int size)
+{
+	if (size <= 0)
+		return true;
+
+	if (isUniqueHelper(arr, size - 1, arr[size - 1]) == false)
+		return false;
+
+	return isUnique(arr, size - 1);
+}
+
+int main(){
+    int arr1[] = {1, 2, 3, 4, 5};
+    int arr2[] = {1, 1, 1, 1, 1};
+
+    cout << isUnique(arr1, 5) << endl;
+    cout << isUnique(arr2, 5) << endl;
+
+    return 0;
+}	
+	
